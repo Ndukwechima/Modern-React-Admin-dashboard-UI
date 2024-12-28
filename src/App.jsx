6055e51cb72d67bf6941e60289657bc45b6135f9
chimae <chimandukwe.r@gmail.com>
2024-12-28 12:26:11 +0000

    Completed the Sidebar of the dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import OverviewPage from "./pages/OverviewPage";
+import ProductsPage from "./pages/ProductsPage";
+import Sidebar from "./components/Sidebar";
 
 const App = () => {
   return (
     <div className="flex h-screen bg-blue-50 text-gray-900 overflow-hidden">
+      {/* BG */}
+      <div className="fixed insect-0 z-0">
+        <div className="absolute inset-0 bg-blue-200 opacity-20" />
+        <div className="absolute inset-0 backdrop-blur-sm" />
+      </div>
+
+
+    {/* Sidebar */}
+    <Sidebar />
       <Routes>
         <Route path="/" element={<OverviewPage />} />
         <Route path="/products" element={<ProductsPage />} />
       </Routes>
     </div>
   );
-}
+};
 
-export default App
+export default App;
