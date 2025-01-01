55dafd9734215936418e73bba06342ea114bd2da
chimae <chimandukwe.r@gmail.com>
2025-01-01 06:05:09 +0000

    Completed sales page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import OverviewPage from "./pages/OverviewPage";
 import ProductsPage from "./pages/ProductsPage";
 import Sidebar from "./components/Common/Sidebar";
 import UsersPage from "./pages/UsersPage";
+import SalesPage from "./pages/SalesPage";
 
 const App = () => {
   return (
@@ -20,6 +21,7 @@ const App = () => {
         <Route path="/" element={<OverviewPage />} />
         <Route path="/products" element={<ProductsPage />} />
         <Route path="/users" element={<UsersPage />} />
+        <Route path="/sales" element={<SalesPage />} />
       </Routes>
     </div>
   );
